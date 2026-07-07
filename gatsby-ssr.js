77ee5b35335f033c9b29680664d4f500488e4c4e
MataRocksbze <matarocksblz@gmail.com>
2026-07-06 22:09:58 -0500

    Add Google Tag Manager
    Added Google Tag Manager integration to SSR.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,40 @@
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+const React = require("react")
+
+exports.onRenderBody = ({
+  setHtmlAttributes,
+  setHeadComponents,
+  setPreBodyComponents,
+}) => {
   setHtmlAttributes({ lang: `en` })
+
+  setHeadComponents([
+    React.createElement("script", {
+      key: "google-tag-manager",
+      dangerouslySetInnerHTML: {
+        __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MNV8ZGVH');
+        `,
+      },
+    }),
+  ])
+
+  setPreBodyComponents([
+    React.createElement("noscript", {
+      key: "google-tag-manager-noscript",
+      dangerouslySetInnerHTML: {
+        __html: `
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNV8ZGVH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `,
+      },
+    }),
+  ])
 }
